62d80b166ee86ebfb6f6513169796e744d04d22f
bannosbian <489357918@qq.com>
2026-01-29 00:31:55 +0800

    测完所有数据填充到generate.py

--- simulator_qw/final_test_code/log/generate.py
+++ simulator_qw/final_test_code/log/generate.py
@@ -5,13 +5,86 @@ import pandas as pd
 os.makedirs('./log', exist_ok=True)
 
 # 定义每个流的参数（贴合你的输出）
+
+# ------------- mine -------------
+# flows = [
+#     {"fid": 0, "avg_queue": 3294.7, "throughput": 58.57},
+#     {"fid": 1, "avg_queue": 3760.3, "throughput": 66.83},
+#     {"fid": 2, "avg_queue": 5763.3, "throughput": 56.85},
+#     {"fid": 3, "avg_queue": 9012.8, "throughput": 57.12},
+#     {"fid": 4, "avg_queue": 2528.1, "throughput": 36.61},
+# ]
+# 重传：11698 18040 62750 72586 6041
+# 效率bps/bit：113.8 113.8 63.1 40.6 94.9
+
+# ------------- SR -------------
+
+# flows = [
+#     {"fid": 0, "avg_queue": 3294.7, "throughput": 58.57},
+#     {"fid": 1, "avg_queue": 3760.3, "throughput": 66.83},
+#     {"fid": 2, "avg_queue": 5969.4, "throughput": 57.23},
+#     {"fid": 3, "avg_queue": 9411.7, "throughput": 59.30},
+#     {"fid": 4, "avg_queue": 2596.3, "throughput": 38.99},
+# ]
+# 重传：11698 18040 61344 72586 6041
+# 效率：113.8 113.8 61.4 40.3 96.1
+
+# ------------- RSR -------------
+# flows = [
+#     {"fid": 0, "avg_queue": 3299.6, "throughput": 54.45},
+#     {"fid": 1, "avg_queue": 3730.9, "throughput": 64.63},
+#     {"fid": 2, "avg_queue": 6736.4, "throughput": 57.65},
+#     {"fid": 3, "avg_queue": 9031.9, "throughput": 58.72},
+#     {"fid": 4, "avg_queue": 2579.6, "throughput": 33.01},
+# ]
+# 重传：11854 18129 62414 70939 5995
+# 效率：105.6 110.9 54.8 41.6 81.9
+
+# ------------- FPS -------------
+# flows = [
+#     {"fid": 0, "avg_queue": 3027.5, "throughput": 50.00},
+#     {"fid": 1, "avg_queue": 3637.8, "throughput": 56.84},
+#     {"fid": 2, "avg_queue": 5559.7, "throughput": 57.36},
+#     {"fid": 3, "avg_queue": 8006.3, "throughput": 53.49},
+#     {"fid": 4, "avg_queue": 2757.9, "throughput": 33.72},
+# ]
+# 重传：13130 19750 60451 70219 6364
+# 效率：105.7 100.0 66.0 42.8 78.2
+
+# ------------- none -------------
+# flows = [
+#     {"fid": 0, "avg_queue": 3294.7, "throughput": 58.57},
+#     {"fid": 1, "avg_queue": 3760.3, "throughput": 66.83},
+#     {"fid": 2, "avg_queue": 5763.3, "throughput": 56.85},
+#     {"fid": 3, "avg_queue": 9707.1, "throughput": 60.20},
+#     {"fid": 4, "avg_queue": 2552.5, "throughput": 35.52},
+# ]
+# 重传：11698 18040 62750 76596 5875
+# 效率：113.8 113.8 63.1 39.7 89.0
+
+# ------------- replicate -------------
+# flows = [
+#     {"fid": 0, "avg_queue": 4220.2, "throughput": 54.89},
+#     {"fid": 1, "avg_queue": 4575.4, "throughput": 56.83},
+#     {"fid": 2, "avg_queue": 5184.9, "throughput": 51.67},
+#     {"fid": 3, "avg_queue": 9365.5, "throughput": 60.51},
+#     {"fid": 4, "avg_queue": 2574.5, "throughput": 39.99},
+# ]
+# 重传：10544 17095 59630 73148 5899
+# 效率：83.2 79.5 63.8 41.3 99.4
+
+# ------------- xor -------------
 flows = [
-    {"fid": 0, "avg_queue": 3294.7, "throughput": 58.57},
-    {"fid": 1, "avg_queue": 3760.3, "throughput": 66.83},
-    {"fid": 2, "avg_queue": 5763.3, "throughput": 56.85},
-    {"fid": 3, "avg_queue": 9012.8, "throughput": 57.12},
-    {"fid": 4, "avg_queue": 2528.1, "throughput": 36.61},
+    {"fid": 0, "avg_queue": 200.0, "throughput": 14.77},
+    {"fid": 1, "avg_queue": 1418.6, "throughput": 18.32},
+    {"fid": 2, "avg_queue": 1766.8, "throughput": 25.21},
+    {"fid": 3, "avg_queue": 6607.7, "throughput": 60.51},
+    {"fid": 4, "avg_queue": 256.4, "throughput": 14.71},
 ]
+# 重传：3466 29649 57020 72544 1942
+# 效率：59.05 82.6 91.3 58.6 367.2
+
+
 
 time_grid = np.arange(0, 15.1, 0.1)
 PKT_SIZE_BYTES = 1250
